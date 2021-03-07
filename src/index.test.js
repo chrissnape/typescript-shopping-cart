284533f682f8cc429a3d9725fe9c5b195a6087de
Chris Snape <chris.snape@lovedby.com>
2021-03-07 20:07:48 +0000

    Add tests

--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,41 @@
 const index = require('./index');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(1 + 2).toBe(3);
+test('1 apple should equal 50', () => {
+  expect(index.checkout(['A99'])).toBe(50);
+});
+
+test('3 apples should equal 130', () => {
+  expect(index.checkout(['A99', 'A99', 'A99'])).toBe(130);
+});
+
+test('1 banana should equal 30', () => {
+  expect(index.checkout(['B15'])).toBe(30);
+});
+
+test('2 bananas should equal 45', () => {
+  expect(index.checkout(['B15', 'B15', 'B15'])).toBe(45);
+});
+
+test('1 cherry should equal 60', () => {
+  expect(index.checkout(['C40'])).toBe(60);
+});
+
+test('1 tangerine should equal 99', () => {
+  expect(index.checkout(['T34'])).toBe(99);
+});
+
+test('5 apples should equal 230', () => {
+  expect(index.checkout(['A99', 'A99', 'A99', 'A99', 'A99'])).toBe(230);
+});
+
+test('3 apples and 2 bananas should equal 175', () => {
+  expect(index.checkout(['A99', 'A99', 'A99', 'B15', 'B15'])).toBe(175);
+});
+
+test('3 apples and 3 bananas should equal 205', () => {
+  expect(index.checkout(['A99', 'A99', 'A99', 'B15', 'B15', 'B15'])).toBe(205);
+});
+
+test('1 cherry and 1 tangerine should equal 159', () => {
+  expect(index.checkout(['T34', 'C40'])).toBe(159);
 });
